69215bc47937a4b2a441a9a70e62baeba41ce196
Felipe Fiuza <felipefiuza801@gmail.com>
2026-05-27 21:27:33 -0300

    Adicionado - Update-checker.py

---
+++ core/update_checker.py
@@ -0,0 +1,83 @@
+import requests
+import subprocess
+
+from tkinter import messagebox
+
+from version import APP_VERSION
+from core.logs import registrar_evento
+
+
+URL_VERSION = (
+    "https://raw.githubusercontent.com/"
+    "fiuzafelipe/"
+    "Gerador-de-xml-completo-sistema-econect/"
+    "main/version.json"
+)
+
+
+def verificar_atualizacao(auto=False):
+
+    try:
+
+        response = requests.get(
+            URL_VERSION,
+            timeout=5
+        )
+
+        if response.status_code != 200:
+            return
+
+        dados = response.json()
+
+        versao_online = dados.get("version")
+
+        download_url = dados.get("download_url")
+
+        # =====================================
+        # TEM UPDATE
+        # =====================================
+
+        if versao_online != APP_VERSION:
+
+            registrar_evento(
+                f"Atualização encontrada: {versao_online}"
+            )
+
+            resposta = messagebox.askyesno(
+                "Atualização disponível",
+                f"Há atualizações disponíveis!\n\n"
+                f"Versão atual: {APP_VERSION}\n"
+                f"Nova versão: {versao_online}\n\n"
+                f"Deseja atualizar agora?"
+            )
+
+            if resposta:
+
+                registrar_evento(
+                    "Iniciando updater..."
+                )
+
+                subprocess.Popen(
+                    [
+                        "updater.exe",
+                        download_url
+                    ]
+                )
+
+                os._exit(0)
+
+        else:
+
+            if not auto:
+
+                messagebox.showinfo(
+                    "Sistema atualizado",
+                    "Não há atualizações disponíveis!\n\n"
+                    "Bom uso."
+                )
+
+    except Exception as erro:
+
+        registrar_evento(
+            f"Erro ao verificar atualização: {erro}"
+        )
